fd3b9569f9e981456bae311dbb8ef6086ae3cc68
Galina Guseva <galaguss@yandex.ru>
2022-05-26 00:11:02 +0500

    ispravleno1

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
-const popup = document.querySelector('.popup')
-const editButton = document.querySelector('.profile__edit-button')
-const closeButton = document.querySelector('.popup__icon-close')
-const userName = document.querySelector('.profile__name')
-const userJob = document.querySelector('.profile__job')
-const nameInput = document.querySelector('.popup__field_name')
-const jobInput = document.querySelector('.popup__field_job')
-const POPUP_OPENED_CLASSNAME = 'popup_opened'
-const formElement = popup.querySelector('.popup__form')
+const popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.popup__icon-close');
+const userName = document.querySelector('.profile__name');
+const userJob = document.querySelector('.profile__job');
+const nameInput = document.querySelector('.popup__field_input_name');
+const jobInput = document.querySelector('.popup__field_input_job');
+const formElement = popup.querySelector('.popup__form');
 
-function openPopup (popupElement) {
-    popupElement.classList.add(POPUP_OPENED_CLASSNAME)
+function togglePopup(popupElement) {
+    popupElement.classList.toggle('popup_opened');
 }
 
-function closePopup(popupElement) {
-    popupElement.classList.remove(POPUP_OPENED_CLASSNAME)
-}  
-
 editButton.addEventListener('click', function() {
-    openPopup(popup)
+    togglePopup(popup);
+    nameInput.focus();
     nameInput.value = userName.textContent;
     jobInput.value = userJob.textContent;
-    })
+    });
 
 closeButton.addEventListener('click', function (evt) {
-    closePopup(popup)
-    evt.preventDefault()
-})
+    togglePopup(popup);
+   });
 
 formElement.addEventListener('submit', function(evt) {
-    evt.preventDefault()
+    evt.preventDefault();
     userName.textContent = nameInput.value;
     userJob.textContent = jobInput.value;
-    closePopup(popup)
-})
+    togglePopup(popup);
+});
