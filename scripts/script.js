3421703704acfa66601702eef111b9eae3cff0e5
Galina Guseva <galaguss@yandex.ru>
2022-05-26 14:23:13 +0500

    popup-fields

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,8 @@ const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__icon-close');
 const userName = document.querySelector('.profile__name');
 const userJob = document.querySelector('.profile__job');
-const nameInput = document.querySelector('.popup__field_input_name');
-const jobInput = document.querySelector('.popup__field_input_job');
+const nameInput = document.querySelector(".popup__field[name='userName']");
+const jobInput = document.querySelector(".popup__field[name='userJob']");
 const formElement = popup.querySelector('.popup__form');
 
 function togglePopup(popupElement) {
