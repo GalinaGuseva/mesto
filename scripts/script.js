d9c4744621458db417d816d56e68c7545c2b61a3
Galina Guseva <galaguss@yandex.ru>
2022-05-31 14:36:09 +0500

    add fad-in

--- scripts/script.js
+++ scripts/script.js
@@ -7,24 +7,50 @@ const nameInput = document.querySelector(".popup__field[name='userName']");
 const jobInput = document.querySelector(".popup__field[name='userJob']");
 const formElement = popup.querySelector('.popup__container');
 
-function togglePopup(popupElement) {
-    popupElement.classList.toggle('popup_opened');
+function openPopup(popupElement) {
+    popupElement.classList.add('popup_opened');
+}
+function closePopup(popupElement) {
+    popupElement.classList.remove('popup_opened');
 }
 
-editButton.addEventListener('click', function() {
-    togglePopup(popup);
+const fadeIn = (el, timeout, display) => {
+    el.style.opacity = 0;
+    el.style.display = display || 'block';
+    el.style.transition = `opacity ${timeout}ms`;
+    setTimeout(() => {
+      el.style.opacity = 1;
+    }, 200);
+  };
+
+  const fadeOut = (el, timeout) => {
+    el.style.opacity = 1;
+    el.style.transition = `opacity ${timeout}ms`;
+    el.style.opacity = 0;
+  
+    setTimeout(() => {
+      el.style.display = 'none';
+    }, timeout);
+  };
+
+
+editButton.addEventListener('click', () => {
+    openPopup(popup);
     nameInput.focus();
+    fadeIn(popup, 1000, 'flex');
     nameInput.value = userName.textContent;
     jobInput.value = userJob.textContent;
     });
 
-closeButton.addEventListener('click', function (evt) {
-    togglePopup(popup);
+closeButton.addEventListener('click', () => {
+    closePopup(popup);
+    fadeOut(popup, 1000);
    });
 
-formElement.addEventListener('submit', function(evt) {
-    evt.preventDefault();
+formElement.addEventListener('submit', function(e) {
+    e.preventDefault();
     userName.textContent = nameInput.value;
     userJob.textContent = jobInput.value;
-    togglePopup(popup);
+    fadeOut(popup, 1000);
+    closePopup(popup);
 });
